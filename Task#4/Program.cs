1272f6c780eafcfd3726bacfee3aecca03476338
Artyom <simstemasss@mail.ru>
2022-12-22 16:42:01 +0300

    3

---
+++ Task#4/Program.cs
@@ -0,0 +1,25 @@
+﻿string [] array = {"1234","1","12f","124412","2"};
+string [] array1 = Array3Simbol(array);
+
+ArrayOut(array1);
+void ArrayOut(string [] array){
+for (int i=0; i<array.Length;i++){
+    System.Console.WriteLine(array[i]);
+}
+}
+
+string [] Array3Simbol(string [] array){
+    int count=0;
+    for(int i=0; i<array.Length;i++){
+        if (array[i].Length<=3)
+        count++;
+    }
+    string [] array1 = new string [count];
+    int j=0;
+    for(int i=0; i<array.Length;i++){
+        if (array[i].Length<=3)
+        {array1[j] = array[i];
+        j++;}
+    }
+    return array1;
+}
